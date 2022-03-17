a3f1433c2de97f122824e935e54b68f31959eba6
Romar Desabille <romardesabille1@gmail.com>
2022-03-17 17:04:38 +0800

    A

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     reactStrictMode: true,
     images: {
-        domains: ['http://localhost:3000', 'res.cloudinary.com'],
+        domains: ['https://courseme.vercel.app', 'res.cloudinary.com'],
     },
     env: {
-        api: 'http://127.0.0.1:8000',
+        api: 'https://backend-324.herokuapp.com',
         userRole: 'userRole',
     },
 }
