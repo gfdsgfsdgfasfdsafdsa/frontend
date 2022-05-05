53cb4926bfbc5da5d49a69a1e2b9badcaa02bdc8
Romar Desabille <romardesabille1@gmail.com>
2022-05-06 04:07:32 +0800

    Update

--- pages/register.js
+++ pages/register.js
@@ -102,8 +102,8 @@ const Register = () => {
                 setEmailExist(true)
             }else if(data?.registered){
                 reset()
-                setInfo({ message: 'Registered successfully you can now login.', error: false, success: true })
-                //setInfo({ message: 'We have sent you an email confirmation. Please confirm to login.', error: false, success: true })
+                //setInfo({ message: 'Registered successfully you can now login.', error: false, success: true })
+                setInfo({ message: 'We have sent you an email confirmation. Please confirm to login.', error: false, success: true })
                 window.location.href = '/register#signup'
             }
             setLoading(false)
